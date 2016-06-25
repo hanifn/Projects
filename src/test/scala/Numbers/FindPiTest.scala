4eaae7ab0e958a57c5adb5d816c5c4b97f35a2c6
Hanif Norman <hanifnorman@gmail.com>
2016-06-26 01:38:46 +0800

    Switched to SBT project and IntelliJ IDEA project

---
+++ src/test/scala/Numbers/FindPiTest.scala
@@ -0,0 +1,14 @@
+package Numbers
+
+import org.scalatest.FunSuite
+
+/**
+  * Created by hanifnorman on 26/6/16.
+  */
+class FindPiTest extends FunSuite {
+
+  test("testCalculatePi") {
+
+  }
+
+}
